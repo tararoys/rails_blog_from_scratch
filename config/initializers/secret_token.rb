af7ac6b835c08f8e2e410c660532b44dbe803d3c
Tara Roys <tlroys@gmail.com>
2014-03-09 21:49:53 -0500

    How do I avoid a PGconnection error when I start a rails project with Postgres?
    Look at this database.yaml file in config, and edit it so that it looks like the one I committed.
    
    I am kicking myself for not having an initial config. FYI, my database yaml looks like this:
    
    development:
      adapter: postgresql
      encoding: unicode
      database: blog_development
      pool: 5
      username:
      password:
    
    test:
      adapter: postgresql
      encoding: unicode
      database: blog_test
      pool: 5
      username:
      password:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '28d6b6fa0c0b9326f676b024327da49740584069a2c18254c480bb2805691e2f9ee5e9abbda7d6bd267dee50e42a5d8680f0f59f3fcfed36aabc010cb30fbc4d'
